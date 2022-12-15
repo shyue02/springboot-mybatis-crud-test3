0703d46dfba0e56a7b8209d5ade3287ed0da268e
unknown <shyue02@naver.com>
2022-12-15 17:16:43 +0900

    LoginDto 수정 / 구매수량 0개 이하 시 상품 구매 막기

--- src/main/java/site/metacoding/firstapp/domain/UserDao.java
+++ src/main/java/site/metacoding/firstapp/domain/UserDao.java
@@ -2,7 +2,6 @@ package site.metacoding.firstapp.domain;
 
 import java.util.List;
 
-import site.metacoding.firstapp.web.dto.request.JoinDto;
 import site.metacoding.firstapp.web.dto.request.LoginDto;
 
 public interface UserDao {
